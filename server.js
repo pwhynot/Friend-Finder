b38a5a1068477433b821629cb0aa893744a7ff82
unknown <pwhynotfilms@gmail.com>
2019-05-22 22:53:30 -0500

    multiple files adjusted

--- server.js
+++ server.js
@@ -3,12 +3,22 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
 // Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/home.html" ));
+});
 
+app.get("/survery", function(req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survery.html" ));
+});
+
+    require("./routes/apiRoutes")(app);
+    require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
