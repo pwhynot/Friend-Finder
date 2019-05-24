c0341f731cab006cc84b4b4fad84e117a6497546
unknown <pwhynotfilms@gmail.com>
2019-05-24 15:20:15 -0500

    friend.js added

---
+++ app/data/friends.js
@@ -0,0 +1,12 @@
+const friends = [
+    {
+        name: "Bill Murray",
+        photo: "https://www.space.ca/wp-content/uploads/2015/08/slime-bill-murray.jpg",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        name: "Dan Aykord",
+        photo: "https://www.thewrap.com/wp-content/uploads/2015/07/dan-aykroyd-ghostbusters.jpg",
+        scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+    }
+]
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,4 +8,4 @@ module.exports = function(app) {
 	app.get('/survey', function(req, res){
 		res.sendFile(path.join(__dirname + "/../public/survey.html"));
 	});
-}
+};
