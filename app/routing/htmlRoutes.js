129878903407cf804cfda8d0a21a1f8a23b5aa41
unknown <pwhynotfilms@gmail.com>
2019-05-24 16:27:27 -0500

    home button added and new html route for errors added

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,11 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname + "/../public/home.html"));
 	});
 
-	app.get('/survey', function(req, res){
-		res.sendFile(path.join(__dirname + "/../public/survey.html"));
+	app.get('/survery', function(req, res){
+		res.sendFile(path.join(__dirname + "/../public/survery.html"));
 	});
+
+	app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 };
