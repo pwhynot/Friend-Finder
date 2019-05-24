1e495913e329acb5ebb401a5f37828b0f131da93
unknown <pwhynotfilms@gmail.com>
2019-05-24 15:09:33 -0500

    routes upadate

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,2 +1,56 @@
 const friends = require("../data/friends");
 const path = require("path");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res){
+        res.status(200).json({message: 'connected.'})
+        res.json(friendsList);
+    });
+
+    app.post("/api/friends", function(req, res){
+        const matches = {
+            "name": "none",
+            "photo": 'none'
+        };
+
+        function sum (array) {
+            const total = 0;
+            for (let n = 0; n < array.length; n++) {
+                total += parseInt(array[n]);
+            }
+            return total;
+        }
+
+        const userTotal = sum(req.body.scores);
+
+        const friendTotal = 0;
+
+        for (let i = 0; i < friendsList.length; i++) {
+            friendTotal = sum(friendsList[i].scores);
+
+            if (friendTotal === userTotal) {
+                matches.name = friendsList[i].name;
+                matches.photo = friendsList[i].photo;
+            }
+        };
+
+        if (matches.name === "none") {
+            const closest = 50;
+
+            for (let i = 0; i < friendsList.length; i++) {
+                friendTotal = sum(friendsList[i].scores);
+                const difference = Math.abs(friendTotal - userTotal);
+                if ( difference <= closest ){
+                    closest = difference;
+                    matches.name = friendsList[i].name;
+                    matches.photo = friendsList[i].photo;
+                };
+            };
+        };
+
+        console.log(matches);
+        res.json(matches);
+
+    });
+
+};
--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('app/public'));
 
-require(".apiRoutes")(app);
-require(".htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 
  app.listen(PORT, function() {
