10fa3a5fc7e52a6ee2cac36672b6a11ba1aa4289
unknown <pwhynotfilms@gmail.com>
2019-05-24 16:01:02 -0500

    new friends added

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,11 +2,21 @@ const friends = [
     {
         name: "Bill Murray",
         photo: "https://www.space.ca/wp-content/uploads/2015/08/slime-bill-murray.jpg",
-        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 5]
     },
     {
         name: "Dan Aykord",
         photo: "https://www.thewrap.com/wp-content/uploads/2015/07/dan-aykroyd-ghostbusters.jpg",
-        scores: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+        scores: [5, 2, 5, 4, 5, 1, 2, 5, 4, 5]
+    },
+    {
+        name: "Ernie Hudson",
+        photo: "https://media.comicbook.com/2019/03/ernie-hudson-1161684-1280x0.jpeg",
+        scores: [5, 4, 5, 4, 5, 3, 3, 3, 5, 5]
+    },
+    {
+        name: "Harold Ramis",
+        photo: "https://www.syfy.com/sites/syfy/files/2018/06/haroldramisghostbusters_2.jpg",
+        scores: [5, 4, 3, 3, 2, 3, 5, 5, 5, 5]
     }
 ]
