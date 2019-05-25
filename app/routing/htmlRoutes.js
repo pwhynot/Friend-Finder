149d32e014fc3c7f939d263c23b0cbd38f1d11d1
unknown <pwhynotfilms@gmail.com>
2019-05-24 20:11:00 -0500

    routes edited

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,12 +2,8 @@ const path = require("path");
 
 
 module.exports = function(app) {
-	app.get("/", function(req, res){
-		res.sendFile(path.join(__dirname + "/../public/home.html"));
-	});
-
-	app.get("/survery", function(req, res){
-		res.sendFile(path.join(__dirname + "/../public/survery.html"));
+	app.get("/survery", function(req, res) {
+		res.sendFile(path.join(__dirname, "/../public/survery.html"));
 	});
 
 	app.get("*", function(req, res) {
